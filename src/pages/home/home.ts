1d745011315c60f46c28a131c24388275e0efb07
alumno Secundaria <al3201@cedi.local>
2018-10-19 09:53:14 -0500

    Agregue imagen a evolve, hubo un error al cambiar de ramas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EvolvePage } from '../evolve/evolve';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  evolve = EvolvePage;
 
   constructor(public navCtrl: NavController) {
 
+
+  }
+
+  clickEImagen()
+  {
+    this.navCtrl.push(this.evolve);
   }
 
 }
