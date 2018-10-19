8c0b654139e5c06c7525dbf89c8caa0320752eb4
alumno Secundaria <al3201@cedi.local>
2018-10-19 09:39:19 -0500

    No se que hice jeje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EvolvePage } from '../pages/evolve/evolve';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EvolvePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EvolvePage
   ],
   providers: [
     StatusBar,
